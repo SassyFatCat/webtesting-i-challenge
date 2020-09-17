a0f8d10c1eafa4ea9acc907f46f5b250fae0bc08
Ryan Lee <therealsassyfatcat@outlook.com>
2020-09-17 16:00:50 -0500

    saving

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,26 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+item.enhancement < 20 ? item.enhancement += 1 : null;
+
+  return {
+    ...item
+  };
 }
 
 function fail(item) {
+  item.enhancement < 15 ? item.durability -= 5
+    : item.enhancement >= 15 ? item.durability -= 15
+    : null;
+  item.enhancement > 16 ? item.enhancement -= 1 : null;
+
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  item.durability = 100;
+  
+  return { ...item};
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,29 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+describe('enhancer.js', () => {
+
+    // Success
+    it('Success', function() {
+        const item = { enhancement: 10 };
+        const result = enhancer.success(item);
+        expect(result.enhancement).toBe(11)
+    });
+
+    // Fail
+    it('Fail', function() {
+        const item = { enhancement: 17, durability: 80 };
+        const result = enhancer.fail(item);
+        
+        expect(result.enhancement).toBe(16);
+        expect(result.durability).toBe(65)
+    });
+
+    // Repair
+    it('Repair', function() {
+        const item = { durability: 1 };
+        const result = enhancer.repair(item);
+
+        expect(result.durability).toBe(100);
+    })
+
+});
